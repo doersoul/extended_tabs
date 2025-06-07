cbed308d05d797fe5a636912d97a5d964e20fa23
doer <doersoult@126.com>
2025-06-07 10:35:06 +0800

    update

--- lib/src/gestures/scroll_physics.dart
+++ lib/src/gestures/scroll_physics.dart
@@ -2,8 +2,10 @@ import 'package:flutter/widgets.dart';
 
 /// todo check, add by doersoul@126.com
 final SpringDescription _spring = SpringDescription.withDampingRatio(
-  mass: 1,
-  stiffness: 512,
+  // mass: 1,
+  // stiffness: 512,
+  mass: 0.08,
+  stiffness: 32,
 );
 
 const ScrollPhysics extendedScrollPhysics = ExtendedScrollPhysics();
